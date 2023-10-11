c2699461f06cd7425a7002d4816920a4f289f749
Abishek77 <18e51a0584@hitam.org>
2023-10-11 20:45:16 +0530

    added the sponser card image

--- Minitgo/src/components/Addslider.jsx
+++ Minitgo/src/components/Addslider.jsx
@@ -5,7 +5,8 @@ import Imgss from './images/best-deal2.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './slider.css';
 import { BiInfoCircle } from "react-icons/bi";
-
+import Shoe from './images/shoe.png';
+import { Link } from 'react-router-dom';
 const Slider = () => {
    
   return (
@@ -13,16 +14,11 @@ const Slider = () => {
         <p className='p-2'>Sponsored <BiInfoCircle className='fa-1'/></p>
     <Row>
   
-        <div className='shadow add'>
-        
-          <Col className='col-md-6 '>
-            
-          </Col>
-
-          <Col className='col-md6'>
-             
-          </Col>
-        </div>
+        <Col className=' col-md-12 add sponsor'>
+         
+          {//<Link to="" className='btn btn-primary rounded-pill button mx-5 d-flex justify-content-center   '>Buy now</Link>
+}
+        </Col>
        
     </Row>
     <br></br>
