5a8814495939a89287e5ec4e005e5b21a976ccec
krishna-chaitanya <121970477+Hemang-krishna@users.noreply.github.com>
2023-10-12 21:14:24 +0000

    orders page updated

--- Minitgo/src/pages/Orders.jsx
+++ Minitgo/src/pages/Orders.jsx
@@ -1,153 +1,136 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import Header from '../components/header'
-import { BiCartDownload } from "react-icons/bi";
-import { BiCheckCircle } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import Imgs from'../components/images/men.jpg'
+import Footer from '../components/Footer'
  
-const imgwidth ={
- width:'75%',
- height:'55vh',
- marginLeft:'7%',
- padding:'20px'
-}
 const OrdersPage = () => {
-  const orders = [
-    {
-      id: 1,
-      name: 'Product A',
-      description: 'This is the description for Product A.',
-      price: 10.99,
-      image: 'https://m.media-amazon.com/images/I/71nxs7jjPFL._AC_UF1000,1000_QL80_.jpg',
-      deliveryStatus: 'delivered',
-      deliveryDate: new Date('2023-07-10'),
-    },
-    {
-      id: 2,
-      name: 'Product B',
-      description: 'This is the description for Product B.',
-      price: 19.99,
-      image: 'https://freepngimg.com/save/2123-black-dress-shirt-png-image/879x1216',
-      deliveryStatus: 'delivered',
-      deliveryDate: new Date('2023-07-12'),
-    },
-    {
-      id: 3,
-      name: 'Product C',
-      description: 'This is the description for Product C.',
-      price: 14.99,
-      image: 'https://media.boohoo.com/i/boohoo/gzz45061_black_xl/womens-black-the-printed-midi-shirt-dress/?w=900&qlt=default&fmt.jp2.qlt=70&fmt=auto&sm=fit',
-      deliveryStatus: 'delivered',
-      deliveryDate: new Date('2023-07-15'),
-    },
-    {
-      id: 4,
-      name: 'Product D',
-      description: 'This is the description for Product D.',
-      price: 9.99,
-      image: 'https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png',
-      deliveryStatus: 'pending',
-      estimatedDeliveryTime: 120,
-    },
-  ];
-
-  const [deliveryTimeRemaining, setDeliveryTimeRemaining] = useState({});
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setDeliveryTimeRemaining((prevRemaining) => {
-        const updatedRemaining = { ...prevRemaining };
-        Object.keys(updatedRemaining).forEach((orderId) => {
-          updatedRemaining[orderId] = Math.max(updatedRemaining[orderId] - 1000, 0);
-        });
-        return updatedRemaining;
-      });
-    }, 1000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  const handleDeliveryTimeChange = (orderId, minutes) => {
-    const updatedRemaining = { ...deliveryTimeRemaining };
-    updatedRemaining[orderId] = minutes * 60 * 1000;
-    setDeliveryTimeRemaining(updatedRemaining);
-  };
-
-  const canReturnOrder = (deliveryDate) => {
-    if (!deliveryDate) return false;
-    const currentDate = new Date();
-    const returnDate = new Date(deliveryDate.getTime() + 5 * 24 * 60 * 60 * 1000); // 5 days later
-    return currentDate <= returnDate;
-  };
-
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / (60 * 1000));
-    return `${minutes} minutes`;
-  };
-
   return (
     <>
       <Header />
-      <Container>
-        <h1 className="mt-4 mb-5 text-dark "><BiCartDownload/>Orders </h1>
-        <Row className="justify-content-center">
-          {orders.map((order) => (
-            <Col key={order.id} md={4} sm={6} xs={12} className="mb-4">
-              <Card className="h-100 shadow border border-primary ">
-                {order.deliveryStatus === 'delivered' && order.deliveryDate && (
-                  <span className='text-danger ' style={{backgroundColor:'#d9dfab', width:'90px', padding:'1px', borderRadius:'2px', marginBottom:'0px'}}>Delivered<span><BiCheckCircle/></span></span>
-                )}
-                <Card.Img style={imgwidth} variant="top" src={order.image} />
-                <Card.Body className="d-flex flex-column">
-                  <div>
-                    <Card.Title>{order.name}</Card.Title>
-                    <Card.Text>{order.description}</Card.Text>
-                    <Card.Text>Total Amount: {order.price}</Card.Text>
-                    {order.deliveryStatus === 'delivered' && order.deliveryDate && (
-                      <Card.Text>Delivery Status: Delivered</Card.Text>
-                    )}
-                    {order.deliveryStatus === 'pending' && order.estimatedDeliveryTime && (
-                      <Card.Text>
-                        Estimated Delivery Time: {formatTime(order.estimatedDeliveryTime)}
-                      </Card.Text>
-                    )}
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <section className="h-100 gradient-custom" style={{ backgroundColor: "" }}>
+        <div className="container py-5 h-100">
+          <div className="row justify-content-center align-items-center h-100">
+            <div className="col-lg-10 col-xl-8">
+              <div className="card" style={{ borderRadius: "10px" }}>
+                <div className="card-header px-4 py-5">
+                  <h5 className="text-muted mb-0">
+                    Thanks for your Order,{" "}
+                    <span style={{ color: "black" }}>Hemang Krishna Chaitanya</span>!
+                  </h5>
+                </div>
+                <div className="card-body p-4">
+                  <div className="d-flex justify-content-between align-items-center mb-4">
+                    <p className="lead fw-normal mb-0" style={{ color: "#d8dfab" }}>
+                      Receipt
+                    </p>
+                    <p className="small text-muted mb-0">
+                      Receipt Voucher : 1KAU9-84UIL
+                    </p>
                   </div>
-                  <div className="mt-auto">
-                    {order.deliveryStatus === 'delivered' && order.deliveryDate && (
-                      <>
-                        {canReturnOrder(order.deliveryDate) ? (
-                          <Button variant="warning" className="mb-2">
-                            Return
-                          </Button>
-                        ) : (
-                          <Button variant="secondary" className="mb-2" disabled>
-                            Return
-                          </Button>
-                        )}
-                      </>
-                    )}
-                    {order.deliveryStatus === 'pending' && order.estimatedDeliveryTime && (
-                      <>
-                        {deliveryTimeRemaining[order.id] ? (
-                          <Card.Text>
-                            Time remaining: {formatTime(deliveryTimeRemaining[order.id])}
-                          </Card.Text>
-                        ) : (
-                          <Button variant="primary" className="mr-2 mb-2">
-                            Track Order
-                          </Button>
-                        )}
-                        <Button variant="secondary" className="mb-2">Give Feedback</Button>
-                      </>
-                    )}
+
+                  <div className="card shadow-0 border mb-4">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col-md-2">
+                          <img
+                            src={Imgs}
+                            className="img-fluid"
+                            alt="Phone"
+                          />
+                        </div>
+                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
+                          <p className="text-muted mb-0">Item Name</p>
+                        </div>
+                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
+                          <p className="text-muted mb-0 small">Item clor</p>
+                        </div>
+                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
+                          <p className="text-muted mb-0 small">Item fabric</p>
+                        </div>
+                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
+                          <p className="text-muted mb-0 small">Qty: 1</p>
+                        </div>
+                        <div className="col-md-2 text-center d-flex justify-content-center align-items-center">
+                          <p className="text-muted mb-0 small">599 RS</p>
+                        </div>
+                      </div>
+                      <hr className="mb-4" style={{ backgroundColor: "#e0e0e0", opacity: 1 }} />
+
+                      <div className="row">
+                      <div className="col-md-2">
+                          <p className="text-muted small">Track Order</p>
+                        </div>
+                        <div className="col-md-10">
+                          <div className="progress" style={{ height: "6px", borderRadius: "16px" }}>
+                            <div className="progress-bar" role="progressbar" style={{ width: "65%", borderRadius: "16px", backgroundColor: "#E4D6D2" }}></div>
+                          </div>
+                          <div className="d-flex justify-content-between mb-1">
+                            <p className="">Out for delivery</p>
+                            <p className="">Delivered</p>
+                          </div>
+
+                          <div className="d-flex justify-content-between pt-2">
+                    <p className="fw-bold mb-0">Order Details</p>
+                    <p className="text-muted mb-0">
+                      <span className="fw-bold me-4">Total</span> 1,198.00
+                    </p>
                   </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+
+                  <div className="d-flex justify-content-between pt-2">
+                    <p className="text-muted mb-0">Invoice Number : 788152</p>
+                    <p className="text-muted mb-0">
+                      <span className="fw-bold me-4">Discount</span> 300.00
+                    </p>
+                  </div>
+
+                  <div className="d-flex justify-content-between">
+                    <p className="text-muted mb-0">Invoice Date : 22 Dec, 2019</p>
+                    <p className="text-muted mb-0">
+                      <span className="fw-bold me-4">GST 18%</span> 150 RS
+                    </p>
+                  </div>
+
+                  <div className="d-flex justify-content-between mb-5">
+                    <p className="text-muted mb-0">Receipt Voucher : 18KU-62IIK</p>
+                    <p className="text-muted mb-0">
+                      <span className="fw-bold me-4">Delivery Charges</span> Free
+                    </p>
+                  </div>
+                  </div>
+
+                  <div className='d-flex align-items-center py-3'>
+
+                  <Link to="/" className="btn btn-primary mx-1" role="button" aria-disabled="">Track</Link>
+
+                  <Link to="/" className="btn btn-light border rounded-pill mx-1" role="button" aria-disabled="">Feedback</Link>
+
+                  <Link to="/" className="btn btn-light border rounded-pill mx-1" role="button" aria-disabled="">Return</Link>
+
+                  <Link to="/" className="btn btn-light border rounded-pill mx-1" role="button" aria-disabled="">Review</Link>
+                  </div>
+
+                 <div className="card-footer border-0 px-4 py-2" style={{ backgroundColor: "#E4D6D2", borderRadius: "50px" }}>
+                  <h5 className="d-flex align-items-center justify-content-end text-dark text-uppercase mb-0">
+                    Total paid: <span className="h2 mb-0 ms-2">1,048 RS</span>
+                  </h5>
+                </div>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       
     </>
   );
